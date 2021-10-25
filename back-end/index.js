41bcb7ffa3ec3512a00cce51d2bed825ca94906f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-25 17:22:23 -0300

    Estrutura inicial pronta

---
+++ back-end/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http:/localhost:3001',
+    methods: ['GET', 'POST']
+  }
+});
+
+http.listen(3001, () => {
+  console.log('Online na 3001')
+})
