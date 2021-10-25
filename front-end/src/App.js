78b6505dc8112fb586370a56ecca5c7101b64b8b
hugolfsimoes <hugolfsimoes@gmail.com>
2021-10-25 17:30:26 -0300

    estrutura basica front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Mural de Depoimentos</h1>
+      <div className="container-mural">
 
+      </div>
+      <form action="">
+        <input type="text" className="input-name" />
+        <input type="text" className="inputmessage" />
+        <button className="send-button">Send</button>
+      </form>
     </div>
   );
 }
